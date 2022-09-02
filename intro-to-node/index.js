e6d38f71b99c82e18452f209c1f6e5b8a40c4e26
harsh123-baba <harshitgoyal213@gmail.com>
2022-09-02 12:07:25 +0530

    superheroes and supervillians

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,1 +1,12 @@
-console.log("Hi, how can you create package,json file using cmd there are no need to create a react app.")
+//jshint esversion:6
+
+// console.log("Hi, how can you create package,json file using cmd there are no need to create a react app.")
+
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+
+var supervillains = require("supervillains");
+var superVillains = supervillains.random();
+console.log(superVillains);
+
